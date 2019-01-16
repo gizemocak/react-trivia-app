f267542966342c4f59c0dec3b96d3ca10cdeb27d
Gizem Ocak <Gizem@Gizems-MacBook-Air.local>
2019-01-16 13:28:04 -0500

    deploying the app

---
+++ build/precache-manifest.4a4a3dfcbe58e1bb44fed218ae1b11d5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "cb0104f2bd66a51a9b88",
+    "url": "/static/js/main.cb0104f2.chunk.js"
+  },
+  {
+    "revision": "2d935040ea16d55f4394",
+    "url": "/static/js/1.2d935040.chunk.js"
+  },
+  {
+    "revision": "cb0104f2bd66a51a9b88",
+    "url": "/static/css/main.966314c6.chunk.css"
+  },
+  {
+    "revision": "6a3ae71616adcf1ebfff961130926a1e",
+    "url": "/index.html"
+  }
+];
